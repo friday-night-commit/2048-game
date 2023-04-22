30eb068cf364481b55d6cff3bf4ca65ee0c0f01e
Julia Aranovich <julia.aranovich@yandex.ru>
2023-04-22 12:57:30 +0400

    Revert "Remove commented code"
    This reverts commit 389ee078282c8dece50c5ff1ed4f5081ba1bd0d5.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,20 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import { useEffect } from 'react'
+import { Button } from '@material-tailwind/react'
 import './App.css'
 
 function App() {
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}`
+  //     const response = await fetch(url)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
+
+  //   fetchServerData()
+  // }, [])
   return (
     <RouterProvider router={router} />
   )
