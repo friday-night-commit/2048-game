b2cd3253b920d123422d7d0141e7498ec820c231
Alexey <dev_perebigaylo@starta.ru>
2023-04-22 12:48:18 +0700

    Remove Button attributes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -5,27 +5,18 @@ import { Button } from '@material-tailwind/react'
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}`
+  //     const response = await fetch(url)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
 
-    fetchServerData()
-  }, [])
+  //   fetchServerData()
+  // }, [])
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Вот тут будет жить ваше приложение :)
-      </h1>
-      <Button nonce='' onResize='' onResizeCapture=''>
-        Кликни на меня
-      </Button>
-
-      <RouterProvider router={router} />
-    </>
+    <RouterProvider router={router} />
   )
 }
 
