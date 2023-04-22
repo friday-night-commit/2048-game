5afdef32b3709c5651fb7d441fb4c6dac16bfee4
Julia Aranovich <julia.aranovich@yandex.ru>
2023-04-22 15:14:20 +0400

    Add 404 and 500 pages

--- packages/client/src/pages/Auth/index.tsx
+++ packages/client/src/pages/Auth/index.tsx
@@ -15,24 +15,24 @@ export default function AuthPage() {
   }
 
   return (
-    <div className="auth-page">
-      <Card color="white" className='p-12 max-w-md w-full' shadow={false}>
-        <Typography variant="h4" color="blue-gray" className="text-center mb-8">
+    <div className='auth-page'>
+      <Card color='white' className='p-12 max-w-md w-full' shadow={false}>
+        <Typography variant='h4' color='blue-gray' className='text-center mb-8'>
           Авторизация
         </Typography>
         <Form
-          className="w-full"
+          className='w-full'
           handlerSubmit={handleLogin}>
-          <div className="mb-4 flex flex-col gap-6">
-            <Input size="lg" label="Логин" name='login' />
-            <Input type="password" size="lg" label="Пароль" name='password'/>
+          <div className='mb-4 flex flex-col gap-6'>
+            <Input size='lg' label='Логин' name='login' />
+            <Input type='password' size='lg' label='Пароль' name='password'/>
           </div>
-          <Button className="mt-6 mb-4" fullWidth type="submit">
+          <Button className='mt-6 mb-4' fullWidth type='submit'>
             Войти
           </Button>
           <Link
             to={routes.registrationPage}
-            className="font-medium text-blue-500 transition-colors hover:text-blue-700 text-center block">
+            className='font-medium text-blue-500 transition-colors hover:text-blue-700 text-center block'>
             Регистрация
           </Link>
         </Form>
--- packages/client/src/router.tsx
+++ packages/client/src/router.tsx
@@ -1,19 +1,22 @@
-import Auth from './pages/Auth'
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
 } from 'react-router-dom'
 
+import Auth from './pages/Auth'
+import NoMatch from './pages/NoMatch'
+
 export enum routes {
-  mainPage = "/",
-  authPage = "/auth",
-  registrationPage = "/registration"
+  mainPage = '/',
+  authPage = '/auth',
+  registrationPage = '/registration'
 }
 
 export default createBrowserRouter(
   createRoutesFromElements([
-    <Route path={routes.mainPage} element="" />,
+    <Route path={routes.mainPage} element='' />,
     <Route path={routes.authPage} element={<Auth />}></Route>,
+    <Route path='*' element={<NoMatch />}></Route>
   ])
 )
