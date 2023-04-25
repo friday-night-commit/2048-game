3b3d751de19b120ed8fb76a404677c1499b35651
Julia Aranovich <julia.aranovich@yandex.ru>
2023-04-25 17:48:37 +0400

    Add Game page

--- packages/client/src/router.tsx
+++ packages/client/src/router.tsx
@@ -7,13 +7,15 @@ import {
 import routes from './routes'
 
 import MainPage from './pages/Main'
-import Auth from './pages/Auth'
+import AuthPage from './pages/Auth'
+import GamePage from './pages/Game'
 import NoMatch from './pages/NoMatch'
 
 export default createBrowserRouter(
   createRoutesFromElements([
     <Route path={routes.mainPage} element={<MainPage />} />,
-    <Route path={routes.authPage} element={<Auth />}></Route>,
-    <Route path='*' element={<NoMatch />}></Route>
+    <Route path={routes.authPage} element={<AuthPage />} />,
+    <Route path={routes.gamePage} element={<GamePage />} />,
+    <Route path='*' element={<NoMatch />} />
   ])
 )
