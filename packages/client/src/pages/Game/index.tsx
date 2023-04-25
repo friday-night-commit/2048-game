1179afd557fe5cb87909425970580035e84bd768
Julia Aranovich <julia.aranovich@yandex.ru>
2023-04-25 18:30:53 +0400

    Add final game modal

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,16 +1,25 @@
 import { Button, Typography } from '@material-tailwind/react'
 import { useState } from 'react'
 import cx from 'classnames'
+import { useNavigate } from 'react-router-dom'
 
 import PageContainer from '../../Components/PageContainer'
 import Game from '../../Components/Game'
+import Modal from '../../Components/Modal'
+import routes from '../../routes'
 
 export default function GamePage() {
-  const [ confirmRestart, setConfirmRestart ] = useState(false);
+  const navigate = useNavigate();
+
+  const [confirmRestart, setConfirmRestart] = useState(false);
+  const [openFinalModal, setOpenFinalModal] = useState(false);
+
+  const handleOpenFinalModal = () => setOpenFinalModal(!openFinalModal);
 
   const handleRestart = () => {
     if (confirmRestart) {
-      restart();
+      // restart();
+      handleOpenFinalModal();
     }
     setConfirmRestart(!confirmRestart);
   }
@@ -30,7 +39,7 @@ export default function GamePage() {
                 Когда одинаковые элементы соединяются, они объединяются в один.
               </Typography>
             </div>
-            <div className="">
+            <div>
               <Button className='game-button small mr-4' disabled>
                 Шаг назад
               </Button>
@@ -42,6 +51,26 @@ export default function GamePage() {
         </div>
         <div className='game-page-container'>
           <Game />
+          <Modal
+            title='Поздравляем! Цель достигнута!'
+            open={openFinalModal}
+            handleOpen={handleOpenFinalModal}
+            className='game-modal'
+          >
+            <div className='text-center'>
+              <Typography variant='h6' className='mb-8 font-normal'>
+                <b>2048</b> в твоем зачете! Продолжай набирать очки и иди на рекорд, или найди свое имя в рейтинге самых успешных игроков!
+              </Typography>
+              <div>
+                <Button className='game-button small mr-4' onClick={() => handleOpenFinalModal()}>
+                  Продолжить игру
+                </Button>
+                <Button className='game-button small' onClick={() => navigate(routes.liderboardPage)}>
+                  Посмотреть рейтинг
+                </Button>
+              </div>
+            </div>
+          </Modal>
         </div>
       </>
     </PageContainer>
