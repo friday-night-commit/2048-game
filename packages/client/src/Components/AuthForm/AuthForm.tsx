9a0c33b49ea186c3a51645af7987998663968dd2
Alexey Perebigaylo <dev_perebigaylo@starta.ru>
2023-04-26 17:56:40 +0700

    FIX: Правка после ревью Удалил компонент Form; Изменил инициализацию обработчиков события у формы; Добавил немного типизации;

--- packages/client/src/Components/AuthForm/AuthForm.tsx
+++ packages/client/src/Components/AuthForm/AuthForm.tsx
@@ -1,49 +1,55 @@
 import { Card, Input, Button, Typography } from '@material-tailwind/react'
-import { Form } from '../Form'
+import { useCallback, ChangeEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { routes } from '../../router'
-import { FormEvent, useState } from 'react'
 
-function AuthForm() {
-  const [formInputsData, setFormInputsData] = useState({
+type Form = {
+  login: string;
+  password: string;
+}
+
+const AuthForm = function() {
+  const [formInputsData, setFormInputsData] = useState<Form>({
     login: '',
     password: '',
   })
-
-  const onSubmit = () => {
+  
+  const onSubmit = useCallback(function(event: React.FormEvent) {
+    event.preventDefault();
     // Temp console log
     console.log(formInputsData);
-  }
+  }, [formInputsData]);
+  
+  const updateInput = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const inputName = event.target.getAttribute('name') as keyof Form;
+    const inputValue = event.target.value;
+  
+    setFormInputsData({
+      ...formInputsData,
+      [inputName]: inputValue,
+    })
+  }, [formInputsData]);
+
 
   return (
     <Card color="white" className="p-12 max-w-md w-full" shadow={false}>
       <Typography variant="h4" color="blue-gray" className="text-center mb-8">
         Авторизация
       </Typography>
-      <Form className="w-full" handleSubmit={onSubmit}>
+      <form className="w-full" onSubmit={onSubmit}>
         <div className="mb-4 flex flex-col gap-6">
           <Input
             size="lg"
             label="Логин"
             name="login"
-            onChange={event => {
-              setFormInputsData({
-                ...formInputsData,
-                login: event.target.value,
-              })
-            }}
+            onChange={updateInput}
           />
           <Input
             type="password"
             size="lg"
             label="Пароль"
             name="password"
-            onChange={event => {
-              setFormInputsData({
-                ...formInputsData,
-                password: event.target.value,
-              })
-            }}
+            onChange={updateInput}
           />
         </div>
         <Button className="mt-6 mb-4" fullWidth type="submit">
@@ -54,7 +60,7 @@ function AuthForm() {
           className="font-medium text-blue-500 transition-colors hover:text-blue-700 text-center block">
           Регистрация
         </Link>
-      </Form>
+      </form>
     </Card>
   )
 }
