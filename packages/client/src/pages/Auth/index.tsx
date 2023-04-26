4f45bbe0f10e36be719ebeb35792fb9c44d5e26e
julia-aranovich <123967434+julia-aranovich@users.noreply.github.com>
2023-04-26 22:46:53 +0400

    Add 404 and 500 pages (#9)

--- packages/client/src/pages/Auth/index.tsx
+++ packages/client/src/pages/Auth/index.tsx
@@ -1,11 +1,10 @@
-import './style.scss'
 import { AuthForm } from '../../Components/AuthForm'
+import CardContainer from '../../Components/CardContainer'
 
 export default function AuthPage() {
-
   return (
-    <div className="auth-page">
+    <CardContainer className='auth-page'>
       <AuthForm />
-    </div>
+    </CardContainer>
   )
 }
