1b4aa86d1832e2c74f29a5548424de6cc64c8ccf
julia-aranovich <123967434+julia-aranovich@users.noreply.github.com>
2023-04-27 17:37:42 +0400

    Create folder for RestartButton component (#15)

---
+++ packages/client/src/pages/Game/components/RestartButton/index.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@material-tailwind/react'
+import { useState, useCallback, FC } from 'react'
+import cx from 'classnames'
+
+type RestartButtonProps = {
+  restart: () => void
+}
+
+const RestartButton: FC<RestartButtonProps> = ({ restart }) => {
+  const [confirmRestart, setConfirmRestart] = useState(false);
+
+  const handleRestart = useCallback(() => {
+    if (confirmRestart) {
+      restart();
+    }
+    setConfirmRestart(!confirmRestart);
+  }, [confirmRestart])
+
+  return (
+    <Button className={cx('game-button', 'small', { alert: confirmRestart })} onClick={() => handleRestart()}>
+      {confirmRestart ? 'Нажми еще раз' : 'Начать заново'}
+    </Button>
+  )
+}
+
+export default RestartButton
