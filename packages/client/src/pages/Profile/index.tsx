d1e7dd0ee853690af7b874b99bd54e1c12a93ef3
a.a.laktionov <a.a.laktionov@we-on.com>
2023-04-27 13:12:13 +0300

    Fixed styles

--- packages/client/src/pages/Profile/index.tsx
+++ packages/client/src/pages/Profile/index.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { FormEvent, useState } from 'react'
 import Input from '../../Components/Input/index'
-import { Button, Card, Typography } from '@material-tailwind/react'
+import { Button, Typography } from '@material-tailwind/react'
 import Avatar from '../../Components/Avatar'
 import routes from '../../routes'
 import CardContainer from '../../Components/CardContainer'
@@ -34,77 +34,74 @@ export default function ProfilePage() {
 
   return (
     <CardContainer>
-      <Card color='white' className='p-12 max-w-md w-full' shadow={false}>
-        <Typography variant='h3' className='text-center mb-8'>
-          Настройки профиля
-        </Typography>
+      <Typography variant='h3' className='text-center mb-8'>
+        Настройки профиля
+      </Typography>
 
-        <div className='mb-8 flex justify-center'>
-          <Avatar alert='photo' />
+      <div className='mb-8 flex justify-center'>
+        <Avatar alert='photo' />
+      </div>
+      <Form className='w-full'
+            handlerSubmit={handleSubmit}>
+        <Input
+          name={UserFields.first_name}
+          type='text'
+          placeholder='Имя'
+          required={true}
+        />
+        <Input
+          name={UserFields.second_name}
+          type='text'
+          placeholder='Фамилия'
+          required={true}
+        />
+        <Input
+          name={UserFields.login}
+          type='text'
+          placeholder='Логин'
+          required={true}
+        />
+        <Input
+          name={UserFields.email}
+          type='email'
+          placeholder='Почта'
+          required={true}
+        />
+        <Input
+          name={UserFields.phone}
+          type='tel'
+          placeholder='+7999888999'
+          required={true}
+        />
+
+        <div className='mt-4 mb-4 flex flex-col'>
+          <Button
+            onClick={() => openChangePasswordModal()}
+            className='small game-button'>
+            Изменить пароль
+          </Button>
         </div>
-        <Form className='w-full'
-              handlerSubmit={handleSubmit}>
-          <Input
-            name={UserFields.first_name}
-            type='text'
-            placeholder='Имя'
-            required={true}
-          />
-          <Input
-            name={UserFields.second_name}
-            type='text'
-            placeholder='Фамилия'
-            required={true}
-          />
-          <Input
-            name={UserFields.login}
-            type='text'
-            placeholder='Логин'
-            required={true}
-          />
-          <Input
-            name={UserFields.email}
-            type='email'
-            placeholder='Почта'
-            required={true}
-          />
-          <Input
-            name={UserFields.phone}
-            type='tel'
-            placeholder='+7999888999'
-            required={true}
-          />
 
-          <div className='mt-4 mb-4 flex flex-col'>
-            <Button
-              onClick={() => openChangePasswordModal()}
-              className='small game-button'>
-              Изменить пароль
-            </Button>
-          </div>
+        <div className='mb-4 flex flex-col'>
+          <Button
+            className='mt-6 mb-4'
+            type='submit'
+          >
+            Сохранить
+          </Button>
+          <Button
+            color='amber'
+            className='mt-2 mb-4'
+            onClick={() => navigate(-1)}
+          >
+            Вернуться Назад
+          </Button>
+        </div>
+      </Form>
+      <ChangePasswordModal handleOpen={handleOpenChangePasswordModal}
+                           onClose={handleOpenChangePasswordModal}
+                           open={visibilityChangePasswordModal} />
 
-          <div className='mb-4 flex flex-col'>
-            <Button
-              className='mt-6 mb-4'
-              type='submit'
-            >
-              Сохранить
-            </Button>
-            <Button
-              color='amber'
-              className='mt-2 mb-4'
-              onClick={() => navigate(-1)}
-            >
-              Вернуться Назад
-            </Button>
-          </div>
-        </Form>
-      </Card>
-      <div> {visibilityChangePasswordModal &&
-        (<ChangePasswordModal handleOpen={handleOpenChangePasswordModal}
-                              onClose={handleOpenChangePasswordModal}
-                              open={visibilityChangePasswordModal} />)
-      }</div>
     </CardContainer>
   )
 }
