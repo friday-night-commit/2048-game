9969a3eadc5581f2ed2d93679ce3b4d599f9cfdf
Дарья <yppix@yandex.ru>
2023-04-30 21:53:19 +0300

    add game mechanics v.1.0

---
+++ packages/client/src/Components/Game/utils/generateRandom.tsx
@@ -0,0 +1,11 @@
+export default function generateRandom(min = 0, max = 100) {
+  const difference = max - min;
+
+  let rand = Math.random();
+
+  rand = Math.floor(rand * difference);
+
+  rand = rand + min;
+
+  return rand;
+}
