fe8f8c891aed24c25513321eb4007241e4acd8b1
Julia Aranovich <julia.aranovich@yandex.ru>
2023-05-02 14:48:53 +0400

    Fix linter errors for server code

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,21 +1,21 @@
-import dotenv from 'dotenv'
-import cors from 'cors'
-dotenv.config()
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
 
-import express from 'express'
-import { createClientAndConnect } from './db'
+import express from 'express';
+import { createClientAndConnect } from './db';
 
-const app = express()
-app.use(cors())
-const port = Number(process.env.SERVER_PORT) || 3001
+const app = express();
+app.use(cors());
+const port = Number(process.env.SERVER_PORT) || 3001;
 
-createClientAndConnect()
+createClientAndConnect();
 
 app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)')
-})
+  res.json('👋 Howdy from the server :)');
+});
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
-})
+  console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
+});
