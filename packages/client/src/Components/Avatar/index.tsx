c1607e3544a67253ebad1e19e424157f49669553
a.a.laktionov <a.a.laktionov@we-on.com>
2023-05-05 12:29:32 +0300

    fixed lint ";"

--- packages/client/src/Components/Avatar/index.tsx
+++ packages/client/src/Components/Avatar/index.tsx
@@ -1,50 +1,52 @@
-import React, { FC, useState } from 'react'
-import Toast from '../Toast'
+import React, { FC, useState } from 'react';
+import Toast from '../Toast';
 import './index.scss';
 
 type TOwnProps = {
   alert?: string;
-}
+};
 
-type TProps = FC<TOwnProps>
+type TProps = FC<TOwnProps>;
 
-const Avatar: TProps = ({alert}) => {
-  const [preview, setPreview] = useState('')
-  const [error, setError] = useState('')
+const Avatar: TProps = ({ alert }) => {
+  const [preview, setPreview] = useState('');
+  const [error, setError] = useState('');
 
   function handleError(e: React.SyntheticEvent<HTMLInputElement>) {
-    console.log('handleError', e)
+    // eslint-disable-next-line no-console
+    console.log('Avatar handleError', e);
   }
 
   function handleUpload(e: React.FormEvent<HTMLInputElement>) {
     if (!e) {
-      return
+      return;
     }
-    const target = e.target as HTMLInputElement
+    const target = e.target as HTMLInputElement;
 
     try {
       if (target.files && target.files.length) {
-        const uri = URL.createObjectURL(target.files[0])
-        setPreview(uri)
+        const uri = URL.createObjectURL(target.files[0]);
+        setPreview(uri);
       }
     } catch (e) {
-      console.log('e', e)
-      const err = (e as Error).message
-      setError(err)
+      const err = (e as Error).message;
+      setError(err);
     }
   }
 
   return (
     <div>
-      <div className='avatar-container'>
-        <img className='avatar-container__image' src={preview} alt={alert} />
-        <label className='avatar-container__file'>
-          <span className='avatar-container__text'>Нажмите чтобы изменить ваш аватар</span>
+      <div className="avatar-container">
+        <img className="avatar-container__image" src={preview} alt={alert} />
+        <label className="avatar-container__file">
+          <span className="avatar-container__text">
+            Нажмите чтобы изменить ваш аватар
+          </span>
           <input
-            type='file'
-            className='avatar-container__input'
-            name='avatar'
-            accept='image/*'
+            type="file"
+            className="avatar-container__input"
+            name="avatar"
+            accept="image/*"
             onChange={el => handleUpload(el)}
             onError={err => handleError(err)}
           />
@@ -52,7 +54,7 @@ const Avatar: TProps = ({alert}) => {
       </div>
       {error && <Toast text={error} />}
     </div>
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
