d908fc8737e15768df73c76d9a0418f11fb28476
a.a.laktionov <a.a.laktionov@we-on.com>
2023-05-22 15:58:16 +0300

    added AudiPlayer

---
+++ packages/client/src/WebAPI/AudiPlayer.ts
@@ -0,0 +1,62 @@
+import Sound from './Sound';
+import notificationSoundUrl from '../assets/sounds/notification.mp3';
+import laserSound from '../assets/sounds/laser.mp3';
+
+type TSound = {
+  name: string;
+  url: string;
+};
+
+export const enum SoundNames {
+  Notification = 'notification',
+  Start = 'start',
+}
+
+export class AudiPlayer {
+  private _sounds: Sound[] = [];
+  private readonly _load: Promise<Sound[]>;
+
+  private _audioContext = new AudioContext();
+
+  constructor() {
+    this._load = Promise.all(
+      listSounds.map(sound =>
+        fetch(sound.url)
+          .then(res => res.arrayBuffer())
+          .then((res: ArrayBuffer) => this._decodeAudioData(res))
+          .then((buffer: AudioBuffer) => {
+            return new Sound(this._audioContext, sound.name, buffer);
+          })
+      )
+    );
+  }
+
+  public async init() {
+    this._sounds = await this._load;
+  }
+
+  public getSoundByName(soundName: string) {
+    const foundedSound = this._sounds.find(sound => sound.name === soundName);
+    if (!foundedSound) {
+      throw new Error(`Не найден звук с именем ${soundName}`);
+    }
+    return foundedSound;
+  }
+
+  private _decodeAudioData(audioData: ArrayBuffer) {
+    return new Promise<AudioBuffer>((resolve, reject) => {
+      this._audioContext.decodeAudioData(audioData, resolve, reject);
+    });
+  }
+}
+
+export const listSounds: TSound[] = [
+  {
+    name: SoundNames.Notification,
+    url: notificationSoundUrl,
+  },
+  {
+    name: SoundNames.Start,
+    url: laserSound,
+  },
+];
--- packages/client/src/WebAPI/notification.service.tsx
+++ packages/client/src/WebAPI/notification.service.tsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { AudiPlayer, SoundNames } from './AudiPlayer';
+import { Button } from '@material-tailwind/react';
 
 type notificationProps = {
   title: boolean;
 };
 
 class DesktopNotification extends Component {
+  private audiPlayer: AudiPlayer;
+
   constructor(props: notificationProps) {
     super(props);
+    this.audiPlayer = new AudiPlayer();
     this.showNotification = this.showNotification.bind(this);
   }
 
   componentDidMount() {
+    this.audiPlayer.init().then();
+
     if (!('Notification' in window)) {
       alert('Браузер не поддерживает Notification API');
     } else if (Notification.permission === 'granted') {
@@ -27,12 +34,16 @@ class DesktopNotification extends Component {
     }
   }
 
+  playSound(soundName: string): void {
+    this.audiPlayer.getSoundByName(soundName).play();
+  }
+
   showNotification() {
     const options: NotificationOptions = {
       body: 'Нажмите, чтобы прочитать',
       icon: 'https://cdn-icons-png.flaticon.com/512/8297/8297354.png?    auto=compress&cs=tinysrgb&dpr=1&w=500',
       dir: 'ltr',
-      vibrate: 5
+      vibrate: 5,
     };
     new Notification('У вас новое сообщение', options);
   }
@@ -40,7 +51,13 @@ class DesktopNotification extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.showNotification}>Показать уведомление</button>
+        <Button className='ml-2 ' onClick={this.showNotification}>Показать уведомление</Button>
+        <Button className='ml-2' onClick={() => this.playSound(SoundNames.Notification)}>
+          Включить звук 1
+        </Button>
+        <Button className='ml-2' onClick={() => this.playSound(SoundNames.Start)}>
+          Включить звук 2
+        </Button>
       </div>
     );
   }
