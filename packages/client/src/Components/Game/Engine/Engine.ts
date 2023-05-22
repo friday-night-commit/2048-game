28ab49f135e9fe0e71878d75055d8c7c9120521c
Дарья <yppix@yandex.ru>
2023-05-22 03:39:55 +0300

    Rebasing and changes to engine

--- packages/client/src/Components/Game/Engine/Engine.ts
+++ packages/client/src/Components/Game/Engine/Engine.ts
@@ -443,7 +443,6 @@ export class Engine {
     if (!this.checkEndGame()) {
       this.render();
     } else {
-      console.log(this.currentMaxNumber);
       if(this.currentMaxNumber >= this.maxValue) {
         this._openSuccess();
       } else {
