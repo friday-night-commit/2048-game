81c0b63d4628340cead608bfc1895270bff7dfe6
Julia Aranovich <julia.aranovich@yandex.ru>
2023-06-08 18:19:48 +0400

    [SSR] Redux store initialization

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,12 +1,15 @@
-import App from './src/App-ssr';
 import { ThemeProvider } from '@material-tailwind/react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { Provider } from 'react-redux';
-import { store } from './src/store';
+
+import App from './src/App-ssr';
+import { createStore } from './src/store';
 
 async function render(uri) {
+  const store = createStore();
   const initialState = store.getState();
+
   const renderResult = renderToString(
     <StaticRouter location={uri}>
       <Provider store={store}>
