37a7949d6ad4a75f440f32d15a81721ff931625a
Дарья <yppix@yandex.ru>
2023-06-14 21:23:49 +0300

    FIx tests

--- packages/server/tailwind.config.js
+++ packages/server/tailwind.config.js
@@ -1,3 +1,15 @@
-import config from 'client/tailwind.config';
+import withMT from '@material-tailwind/react/utils/withMT';
 
-export default config;
+/** @type {import('tailwindcss').Config} */
+export default withMT({
+  content: [
+    '.../../index.html',
+    '../../packages/client/src/**/*.{ts,tsx}',
+    '../../node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    '../../node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
