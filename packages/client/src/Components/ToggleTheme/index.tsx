3227a0e9c02b0fe491600154b7e6be1414bad40c
Дарья <yppix@yandex.ru>
2023-06-24 20:24:14 +0300

    Saving in local storage

--- packages/client/src/Components/ToggleTheme/index.tsx
+++ packages/client/src/Components/ToggleTheme/index.tsx
@@ -1,9 +1,20 @@
-import React, { useState,useContext } from 'react';
+import React, { useState } from 'react';
 import { FaSun, FaMoon } from 'react-icons/fa';
-import { ThemeContext } from '../../Utils/ThemeContext';
+
+const getInitialTheme = () => {
+  if (typeof window !== 'undefined' && window.localStorage) {
+
+    const storedPrefs = window.localStorage.getItem('color-theme');
+    if (storedPrefs) {
+      return storedPrefs;
+    }
+  }
+
+  return 'light';
+};
 
 const ToggleTheme = () => {
-  const [theme, setTheme] = useState(useContext(ThemeContext));
+  const [theme, setTheme] = useState(getInitialTheme);
 
   if (typeof window !== 'undefined') {
     const root = window.document.documentElement;
@@ -11,6 +22,8 @@ const ToggleTheme = () => {
 
     root.classList.remove(isDark ? 'light' : 'dark');
     root.classList.add(theme);
+
+    localStorage.setItem('color-theme', theme);
   }
   return (
     <div className='toggle-button'>
