fafaf985978322c41c8e4a106653213c8f9b258c
yppix <38808605+yppix@users.noreply.github.com>
2023-06-26 22:31:10 +0300

    Fri 49 (#51)
    Add change theme on client

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
-import { ThemeProvider } from '@material-tailwind/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -11,6 +10,7 @@ import { YandexAPIRepository } from './repository/YandexAPIRepository';
 import { UserService } from './api/UserService';
 
 import './index.scss';
+import { ThemeProvider } from './Utils/ThemeProvider';
 
 let initialState = {};
 try {
@@ -28,13 +28,13 @@ delete window.__REDUX_INITIAL_STATE__;
 hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <React.StrictMode>
+    <ThemeProvider>
     <BrowserRouter>
       <Provider store={store}>
-        <ThemeProvider>
           <App />
-        </ThemeProvider>
       </Provider>
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
