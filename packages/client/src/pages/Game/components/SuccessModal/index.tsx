a0a7dd92f7d09e9bf391328c3907ff662d2af446
julia-aranovich <123967434+julia-aranovich@users.noreply.github.com>
2023-07-02 23:04:36 +0400

    Fix for the game result modals (#61)

--- packages/client/src/pages/Game/components/SuccessModal/index.tsx
+++ packages/client/src/pages/Game/components/SuccessModal/index.tsx
@@ -1,11 +1,12 @@
 import { Button, Typography } from '@material-tailwind/react';
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Modal from '../../../../Components/Modal';
 
 import routes from '../../../../routes';
 import { useAppDispatch } from '../../../../hooks/redux';
+import { closeModalSuccess, renewMatrix } from '../../../../store/slices/Modal';
 
 type ModalProps = {
   open: boolean;
@@ -16,7 +17,15 @@ const SuccessModal: FC<ModalProps> = ({ open, handleOpen }) => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  //const handleContinuePlay = () => dispatch(continuePlay());
+  const restart = useCallback(() => {
+    dispatch(renewMatrix());
+    dispatch(closeModalSuccess());
+  }, []);
+
+  const goToLeaderboard = useCallback(() => {
+    dispatch(closeModalSuccess());
+    navigate(`/${routes.leaderboardPage}`);
+  }, []);
 
   return (
     <Modal
@@ -26,18 +35,14 @@ const SuccessModal: FC<ModalProps> = ({ open, handleOpen }) => {
       className='game-modal'>
       <div className='text-center'>
         <Typography variant='h6' className='mb-8 font-normal'>
-          <b>2048</b> в твоем зачете! Продолжай набирать очки и иди на рекорд,
-          или найди свое имя в рейтинге самых успешных игроков!
+          <b>2048</b> в твоем зачете! Попробуй еще раз (за меньшее число шагов
+          ;) ), или найди свое имя в рейтинге самых успешных игроков!
         </Typography>
         <div>
-          {/*<Button*/}
-          {/*  className='game-button small mr-4'*/}
-          {/*  onClick={ handleContinuePlay }>*/}
-          {/*  Продолжить игру*/}
-          {/*</Button>*/}
-          <Button
-            className='game-button small'
-            onClick={() => navigate(`/${routes.leaderboardPage}`)}>
+          <Button className='game-button small mr-4' onClick={restart}>
+            Начать сначала
+          </Button>
+          <Button className='game-button small' onClick={goToLeaderboard}>
             Посмотреть рейтинг
           </Button>
         </div>
