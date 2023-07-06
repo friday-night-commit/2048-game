4c2235495d65877302c36878c13458018c67a179
a.a.laktionov <a.a.laktionov@we-on.com>
2023-07-06 09:48:55 +0300

    commented bodyParser

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -13,16 +13,16 @@ import { dbConnect } from './db';
 import apiRouter from './api';
 import multer from 'multer';
 import fs from 'fs';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
 const isDev = process.env.NODE_ENV === 'development';
 
 async function startServer() {
   const port = Number(process.env.SERVER_PORT) || 5000;
 
-  const app = express().use(cookieParser()).use(cors())
-    .use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
-    .use(bodyParser.json({ limit: '50mb' }));
+  const app = express().use(cookieParser()).use(cors());
+    // .use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+    // .use(bodyParser.json({ limit: '50mb' }));
 
   const vite = await initVite(app);
 
