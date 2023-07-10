e7b323494f030288967afa1f4d52afce4658377b
Дарья <yppix@yandex.ru>
2023-07-10 19:39:57 +0300

    Finish theme

--- packages/client/src/Components/ThemeToggler/index.tsx
+++ packages/client/src/Components/ThemeToggler/index.tsx
@@ -2,10 +2,7 @@ import React, { useCallback, useContext } from 'react';
 import { FaSun, FaMoon } from 'react-icons/fa';
 import { ThemeContext } from '../../Utils/ThemeProvider';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
-import { getThemeByName, getUserTheme, updateUserTheme } from '../../store/slices/Theme'
-import { getReactionsByPostId } from '../../store/slices/Reaction'
-import { Reaction } from '../../pages/Forum/forum.interfaces'
-import { Theme, ThemeUser } from './theme.interfaces'
+import { getThemeByName, updateUserTheme } from '../../store/slices/Theme';
 
 const ThemeToggler = () => {
   const { theme, setTheme } = useContext(ThemeContext);
@@ -13,20 +10,14 @@ const ThemeToggler = () => {
    const dispatch = useAppDispatch();
    const user = useAppSelector(store => store.userSlice.user);
 
-  // dispatch(getReactionsByPostId(Number(topicId))).then(data => {
-  //   const reactions = data.payload as Reaction[];
-  //   if (reactions.length) {
-  //     setReactions(reactions);
-  //   }
-  // });
-
   const settingTheme = (color: string) => useCallback(
     function () {
       dispatch(getThemeByName(color)).then(data => {
+        // @ts-ignore
         const themeId = data.payload.id;
-        const userId = user.id;
         if(themeId && user) {
-          dispatch(updateUserTheme({ themeId, userId } as ThemeUser)). then(()=>
+          const userId = user.id;
+          dispatch(updateUserTheme({ themeId, userId })). then(()=>
             {
               setTheme(color);
             }
